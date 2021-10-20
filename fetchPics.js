f97744ab248ba69d7f30b69651ee2f8efb688c8f
alexeicoreiba <alexei.coreiba@gmail.com>
2021-10-20 16:01:37 -0700

    Added cname

--- fetchPics.js
+++ fetchPics.js
@@ -4,7 +4,6 @@ function fetchPics(folder) {
         success: function (data) {
             $(data).find("a").attr("href", function (i, val) {
                 if (val.match(/\.(JPG)$/)) {
-                    console.log('heh');
                     $(".grid").append("<div class='grid-item'><img src='" + val + "'></div>");
                 }
             });
