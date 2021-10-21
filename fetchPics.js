b6522a2883f4c80daa8ee90b99b69fa406c0f6c7
alexeicoreiba <alexei.coreiba@gmail.com>
2021-10-21 10:50:39 -0700

    experimental cors

--- fetchPics.js
+++ fetchPics.js
@@ -2,8 +2,17 @@ function fetchPics(folder) {
     return $.ajax({
         url: folder,
         dataType: "html",
+        crossDomain: true,
+        headers: {
+            "accept": "application/json",
+            'Access-Control-Allow-Origin': "*"
+        },
         success: function (data) {
+            data = JSON.parse(data);
             console.log("data: " + data);
+            var collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
+            data = data.sort(collator.compare).reverse();
+            console.log("data2: " + data);
             $(data).find("a").attr("href", function (i, val) {
                 console.log("val: " + val);
                 if (val.match(/\.(JPG)$/)) {
