961153fcb81b943c1074049b4856a942a176b7ee
alexeicoreiba <alexei.coreiba@gmail.com>
2021-10-21 09:36:21 -0700

    added logger to fetchpics

--- fetchPics.js
+++ fetchPics.js
@@ -3,6 +3,7 @@ function fetchPics(folder) {
         url: folder,
         success: function (data) {
             $(data).find("a").attr("href", function (i, val) {
+                console.log("val: " + val);
                 if (val.match(/\.(JPG)$/)) {
                     $(".grid").append("<div class='grid-item'><img src='" + val + "'></div>");
                 }
